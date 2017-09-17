d11d8f9e2d6357e382c366e29398a929d20f9a74
Potatano <migle.vaituleviciute@gmail.com>
2017-09-17 13:50:53 +0300

    Main methods

---
+++ Skaitiniai1/Algorithm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skaitiniai1
+{
+    class Algorithm
+    {
+        public float Function(float x)
+        {
+            float y;
+            y = x + 1 - (float)Math.Tan(2 * x);
+            return y;
+        }
+
+        public bool CheckInterval(float a, float b)
+        {
+            if (Function(a) * Function(b) < 0)
+                return true;
+            return false;
+        }
+
+        public float FindCenterPoint(float a, float b)
+        {
+            float c;
+            c = (a + b) / 2;
+            return c;
+        }
+
+        public bool IsRangeSmallerTwoEpsilon(float a, float b, float epsilon)
+        {
+            float range = Math.Abs(a) - Math.Abs(b);
+            if (range <= 2 * epsilon)
+                return true;
+            return false; // range IS smaller than two epsilon
+        }
+
+        public bool IsAnswerSmallerEpsilon(float c, float epsilon)
+        {
+            if (Math.Abs(c) <= epsilon)
+                return true;
+            return false; // answer IS smaller than epsilon
+        }
+    }
+}
---
+++ Skaitiniai1/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skaitiniai1
+{
+    class Variables
+    {
+        public float A { get; set; }
+        public float B { get; set; }
+        private float epsilon;
+    }
+}
